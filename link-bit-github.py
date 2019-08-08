0eacb4e085f755c1a116f90a8ea35c3a23d42174
Kenechi Dukor <32472950+kennydukor@users.noreply.github.com>
2019-08-08 11:35:37 +0100

    Some previous commit

--- link-bit-github.py
+++ link-bit-github.py
@@ -3,12 +3,12 @@ import git
 from git_contributions_importer import *
 
 # Your private repo or Bitbucket repo
-repo = git.Repo("C:\WAPXCONNECT\kiosk")
+repo = git.Repo("C:\WAPXCONNECT\wapicins")
 # Your mock repo
 mock_repo = git.Repo("C:\WAPXCONNECT\mock-repo")
 importer = Importer([repo], mock_repo)
 # I use both my personal email and work email here,
 # Since the private repo uses work email, and Github profiles uses
 # my work email
-importer.set_author(['kennydukor@gmail.com', '32472950+kennydukor@users.noreply.github.com'])
+importer.set_author(['kennydukor@gmail.com', 'toyyibazeez@gmail.com'])
 importer.import_repository()
